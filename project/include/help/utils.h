34a95b869eac304af05b52d4605aad90180e6c78
Jorge López <jorge2402@gmail.com>
2017-06-19 21:43:49 +0200

    + Translated comments to English + Added a couple of string utilities

--- project/include/help/utils.h
+++ project/include/help/utils.h
@@ -56,27 +56,48 @@ struct cmp_c_str
 // STRING FUNCTIONS
 //-----------------------------------------------------------------------
 
-inline std::string GetExtension (const std::string& file)
+inline
+std::string GetFileExtension (const std::string& filename)
 {
-	std::string::size_type pos = file.find_last_of('.');
+	std::string::size_type pos = filename.find_last_of('.');
 	
 	// no file extension at all
 	if( pos == std::string::npos)
 		return "";
 	
-	std::string ret = file.substr(pos+1);
+	std::string ret = filename.substr(pos+1);
 	std::transform(ret.begin(),ret.end(),ret.begin(),::tolower); // thanks to Andy Maloney for the hint
 	return ret;
 }
 
 /**
- * Plantilla para convertir datos en cadenas de caracteres
+ * Returns a file name without extension. It uses dynamic memory so you
+ * have to call free() when done with the char array
+ */
+inline
+char* GetFileExtension(const char* filename)
+{
+	char* pch = strrchr(filename, '.');
+	size_t lastIndex = pch - filename;
+	char* filenameNoExt = strndup(filename, lastIndex);
+	return filenameNoExt;
+}
+
+inline
+void EraseAll(std::string& str, const char toRemove)
+{
+	str.erase(std::remove(str.begin(), str.end(), toRemove), str.end());
+}
+
+/**
+ * Template to transform data in a string of characters
  *
  * @param data
- * @return string, una cadena de caracteres
+ * @return string, the actual string of characters
  */
 template <class C>
-inline std::string Data2string(const C& data)
+inline
+std::string Data2string(const C& data)
 {
 	std:: stringstream flow;
 	flow << data;
@@ -89,7 +110,7 @@ inline const bool IsInString(const std::string& strString, const std::string& st
 	if(strString.length() <= 0 || strSubString.length() <= 0)
 		return false;
 	
-	// Si no se encuentra en la cadena, devolvera -1
+	// If it's not in the string it return -1
 	const int index = (int)strString.find(strSubString);
 	
 	return (0 <= index && index < (int)strString.length());
